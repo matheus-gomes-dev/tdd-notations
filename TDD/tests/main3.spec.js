7ac7e6423bb6d407475006f2cea5ac9fb09c4619
matheus-lima92 <lima.eca@gmail.com>
2018-04-02 22:46:46 -0300

    initial tests with calculator example

--- TDD/tests/main3.spec.js
+++ TDD/tests/main3.spec.js
@@ -1,3 +1,5 @@
+/*
+
 var expect = require('chai').expect;
 
 describe('Main', function() {
@@ -58,3 +60,5 @@ describe('Main', function() {
 // test 2
 // afterEach
 // after
+
+*/
---
+++ TDD/tests/main4.spec.js
@@ -0,0 +1,33 @@
+var expect = require('chai').expect;
+var calc = require('../src/main.js');
+
+describe('Calc', function(){
+
+	// smoke tests
+	describe('Smoke tests', function() {
+
+		it('should exist the calc lib', function(){
+			expect(calc).to.exist;
+		})
+
+		it('should exist the method "sum"', function(){
+			expect(calc.sum).to.exist;
+			expect(calc.sum).to.be.a('function');
+		})
+
+		it('should exist the method "sub"', function(){
+			expect(calc.sub).to.exist;
+			expect(calc.sub).to.be.a('function');
+		})
+
+		it('should exist the method "mult"', function(){
+			expect(calc.mult).to.exist;
+			expect(calc.mult).to.be.a('function');
+		})
+
+		it('should exist the method "div"', function(){
+			expect(calc.div).to.exist;
+			expect(calc.div).to.be.a('function');
+		})
+	})
+})
