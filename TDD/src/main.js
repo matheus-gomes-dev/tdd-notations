85c3263562f9e80b09e0a99a4c43d4b6193afd27
matheus-lima92 <lima.eca@gmail.com>
2018-04-04 00:27:26 -0300

    mandatory test coverage before git push

--- TDD/src/main.js
+++ TDD/src/main.js
@@ -4,5 +4,13 @@ const sub = (num1, num2) => num1-num2
 const mult = (num1, num2) => num1*num2
 const div = (num1, num2) => (num2 === 0) ? 'Não é possível divisão por zero!' : num1/num2
 
+/* --- descomentar funções abaixo fará com que seja impossível --- */
+/* --- fazer o push, devido ao teste de coverage que exige 80% --- */
+/* --- 80% das funções testadas --- */
+const func1 = (num1, num2) => num1+1
+const func2 = (num1, num2) => num1+2
+const func3 = (num1, num2) => num1+3
+const func4 = (num1, num2) => num1+4
+
 
 export {sum, sub, mult, div}
